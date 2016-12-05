736fdd35eb7cb3c58a1e59ff31368ea04cf6bf6c
Gustavo Moreira <gustavomoreira@google.com>
2016-12-05 14:05:11 -0800

    First official commit
    Change-Id: I5c145244e943ff753ce5dc2d2ea7061f9828b6b4

--- app.js
+++ app.js
@@ -17,19 +17,21 @@
 
 process.env.DEBUG = 'actions-on-google:*';
 
-const ActionsSdkAssistant = require('./actions-on-google').ActionsSdkAssistant;
+const ActionsSdkAssistant = require('actions-on-google').ActionsSdkAssistant;
 const Eliza = require('elizabot');
 const express = require('express');
 const bodyParser = require('body-parser');
-
-const RAW_INTENT = 'raw.input';
 
 const app = express();
 app.set('port', (process.env.PORT || 8080));
 app.use(bodyParser.json({type: 'application/json'}));
 
+/**
+ * Handles the main intent coming from Assistant, when the user first engages with the action,
+ * asking for an initial prompt from Eliza.
+ */
 const mainIntentHandler = (assistant) => {
-  console.log('mainIntent');
+  console.log('Main intent triggered.');
   const eliza = new Eliza();
   assistant.ask(eliza.getInitial(),
     [{'intent': RAW_INTENT}],
@@ -37,17 +39,24 @@ const mainIntentHandler = (assistant) => {
     {elizaInstance: eliza});
 };
 
+/**
+ * Handles the intent where the user sends a query to be handled by Eliza.
+ *
+ * This intent is triggered inside the dialogs when the user already has a conversation going on
+ * with Eliza, or when the user 'deep links' into the action by calling Eliza and already sending an
+ * initial prompt.
+ */
 const rawIntentHandler = (assistant) => {
-  console.log('rawInput');
+  console.log('Raw input intent triggered.');
   const eliza = new Eliza();
+
+  // Reloads the previous instance of Eliza in case this is an ongoing conversation
   const previousEliza = assistant.getDialogState().elizaInstance;
   if (previousEliza) {
     eliza.quit = previousEliza.quit;
     eliza.mem = previousEliza.mem;
     eliza.lastChoice = previousEliza.lastChoice;
   }
-
-  console.log('RAW INPUT: ' + assistant.getRawInput());
 
   const elizaReply = eliza.transform(assistant.getRawInput());
   if (eliza.quit) {
@@ -60,20 +69,30 @@ const rawIntentHandler = (assistant) => {
   }
 };
 
-app.post('/', function (request, response) {
-  console.log('handle post');
+// Map that contains the intents and respective handlers to be used by the actions client library
+const actionMap = new Map();
+
+// Intent constants
+const RAW_INTENT = 'raw.input';
+
+/**
+ * Configures the post request handler by setting the intent map to the right functions.
+ */
+actionMap.set(new ActionsSdkAssistant().StandardIntents.MAIN, mainIntentHandler);
+actionMap.set(RAW_INTENT, rawIntentHandler);
+
+/**
+ * Handles the post request incoming from Assistant.
+ */
+app.post('/', (request, response) => {
+  console.log('Incoming post request...');
   const assistant = new ActionsSdkAssistant({request: request, response: response});
-
-  const actionMap = new Map();
-  actionMap.set(assistant.StandardIntents.MAIN, mainIntentHandler);
-  actionMap.set(RAW_INTENT, rawIntentHandler);
-
   assistant.handleRequest(actionMap);
 });
 
 // Start the server
-const server = app.listen(app.get('port'), function () {
-  console.log('App listening on port %s', server.address().port);
+const server = app.listen(app.get('port'), () => {
+  console.log('Eliza endpoint listening on port %s', server.address().port);
   console.log('Press Ctrl+C to quit.');
 });
 // [END app]
